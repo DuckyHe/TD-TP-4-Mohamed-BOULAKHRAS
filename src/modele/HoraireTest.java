5989db04a6a0dc8ca6671dfabe70e749386fe209
DuckyHe <mohamed.boulakhras@icloud.com>
2025-04-28 16:51:26 +0200

    Ajout des tests unitaires pour toMinutes et mise en place de l'ordre d'exécution des tests

--- src/modele/HoraireTest.java
+++ src/modele/HoraireTest.java
@@ -3,7 +3,9 @@ package modele;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-class HoraireTest {
+@TestMethodOrder( MethodOrderer.OrderAnnotation.class)
+public class HoraireTest {
+
 
     @BeforeEach
     void setUp() {
@@ -12,7 +14,17 @@ class HoraireTest {
     @AfterEach
     void tearDown() {
     }
-
+    @Order(1)
+    @Test
+    public void testToMinutes() {
+        Horaire h = new Horaire(8, 30);
+        assertEquals(510, h.toMinutes(), "8h30 doit donner 510 minutes");
+        Horaire h1 = new Horaire(12, 0);
+        assertEquals(720, h1.toMinutes(), "12h00 doit donner 720 minutes");
+        Horaire h2 = new Horaire(23, 59);
+        assertEquals(1439, h2.toMinutes(), "23h59 doit donner 1439 minutes");
+    }
+    @Order(2)
     @Test
     void compareTo() {
             Horaire h1 = new Horaire(14, 0); // 14h00
@@ -21,7 +33,6 @@ class HoraireTest {
             Assertions.assertTrue(h2.compareTo(h1) < 0, "h1 doit être avant h2");
             Horaire h3 = new Horaire(9, 30); // 09h30
             Horaire h4 = new Horaire(9, 30); // 09h30
-
             Assertions.assertEquals(0, h3.compareTo(h4));
         }
 
